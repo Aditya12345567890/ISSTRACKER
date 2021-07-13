72e03bd9da9403bd642759f67307b04fb7fd6772
Aditya1234567890 <aditya2naresh@gmail.com>
2021-07-13 11:32:50 -0400

    c78

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import HomeScreen from './screens/Home';
+import IssLocationScreen from './screens/ISSLocationScreen';
+import MeteorScreen from './screens/MeteorScreen';
+import UpdateScreen from './screens/UpdateScreen';
+ 
+
+
+const Stack = createStackNavigator()
+ function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName = 'Home' screenOptions = {{headerShown:false}}>
+       <Stack.Screen name = 'Home' component  = {HomeScreen}/>
+       <Stack.Screen name = 'IssLocation' component  = {IssLocationScreen}/>
+       <Stack.Screen name = 'Meteor' component  = {MeteorScreen}/>
+       <Stack.Screen name = 'Update' component  = {UpdateScreen}/>
+</Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
